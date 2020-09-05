2eb699fdeac4a53df9b9c120527dbe84f9cf5179
EduFernandesUNB <160027136@aluno.unb.br>
2020-09-05 10:09:59 -0300

    Commit

--- configDatabase.js
+++ configDatabase.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg')
 module.exports = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'api',
+    database: 'postgres',
     password: '32599344',
     port: 5432,
   })
