e8e85e41d4e250c0ed613b77b7b597ea7829493e
EduFernandesUNB <160027136@aluno.unb.br>
2020-11-22 18:38:04 -0300

    Ajustes

--- configDatabase.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg')
-
-module.exports = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '32599344',
-    port: 5432,
-  })
